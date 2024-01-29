f7ef7dbb512f8f95fee9e3b52d6cb8c7fea13ff2
Noah Zajicek <noahzajicek@gmail.com>
2024-01-28 23:02:24 -0500

    Create new authMiddleware function for user API key

--- middleware.go
+++ middleware.go
@@ -1,6 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/zajicekn/Blog-Aggregator/internal/database"
+)
+
+type authedHandler func(http.ResponseWriter, *http.Request, database.User)
 
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -13,4 +20,29 @@ func middlewareCors(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func (cfg *apiConfig) middlewareAuth(handler authedHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Get the ApiKey
+		apiKey, err := cfg.getAuthHeader(w, r)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid ApiKey")
+			return
+		}
+
+		// Validate the ApiKey and retrieve the user
+		user, err := cfg.DB.GetUserApiKey(r.Context(), apiKey)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				respondWithError(w, http.StatusNotFound, "user not found")
+			} else {
+				respondWithError(w, http.StatusInternalServerError, "error retrieving user")
+			}
+			return
+		}
+
+		// Call the next handler with the user's information
+		handler(w, r, user)
+	}
 }
